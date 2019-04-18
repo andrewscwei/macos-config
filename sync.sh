f9d19ef1ee32fc0d558834e46115c2fee9adad44
Andrew Wei <andrewscwei@gmail.com>
2019-04-18 09:45:21 -0700

    Update presets

--- sync.sh
+++ sync.sh
@@ -2,12 +2,12 @@
 
 COLOR_PREFIX="\x1b["; COLOR_RESET=$COLOR_PREFIX"0m"; COLOR_BLACK=$COLOR_PREFIX"0;30m"; COLOR_RED=$COLOR_PREFIX"0;31m"; COLOR_GREEN=$COLOR_PREFIX"0;32m"; COLOR_ORANGE=$COLOR_PREFIX"0;33m"; COLOR_BLUE=$COLOR_PREFIX"0;34m"; COLOR_PURPLE=$COLOR_PREFIX"0;35m"; COLOR_CYAN=$COLOR_PREFIX"0;36m"; COLOR_LIGHT_GRAY=$COLOR_PREFIX"0;37m"
 
-echo -e "Exporting VSCode presets..."
+echo -e "${COLOR_PURPLE}Exporting VSCode presets...${COLOR_RESET}"
 cd vscode
 sh ./export.sh
 git add -A && git commit -m "Update presets" && git push
 cd ..
-echo -e "Exporting VSCode presets... ${COLOR_GREEN}OK${COLOR_RESET}"
+echo -e "${COLOR_PURPLE}Exporting VSCode presets... ${COLOR_GREEN}OK${COLOR_RESET}"
 
 echo
 
