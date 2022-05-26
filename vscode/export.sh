289faae75834f729f1ec9dadfb4dd2efedf4a9ae
Andrew Wei <andrewscwei@gmail.com>
2022-05-26 15:45:24 -0700

    Update config

--- vscode/export.sh
+++ vscode/export.sh
@@ -13,4 +13,4 @@ echo -e "Exporting ${COLOR_CYAN}keybindings.json${COLOR_RESET}..."
 cp -rf "${FROM_DIR}/keybindings.json" "${TO_DIR}/keybindings.json"
 
 echo -e "Exporting extensions to ${COLOR_CYAN}extensions.list${COLOR_RESET}..."
-code --list-extensions > ${TO_DIR}/extensions.list
+code --list-extensions > "${TO_DIR}/extensions.list"
