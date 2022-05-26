717fac097e8a5293363839b2644876161c5b4c77
Andrew Wei <andrewscwei@gmail.com>
2022-05-26 15:49:59 -0700

    Update config

--- zsh/export.sh
+++ zsh/export.sh
@@ -5,7 +5,7 @@ CWD=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
 source "${CWD}/../scripts/init.sh"
 
 function main() {
-  export_file ".zshrc" $HOME $CWD/files
+  export_file ".zshrc" "${HOME}" "${CWD}/files"
 }
 
 main
