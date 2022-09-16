574ca77c75fba137c4b14eb07aa3b5ccdaaf20ba
Andrew Wei <andrewscwei@gmail.com>
2022-09-15 21:58:16 -0700

    Update install script for keyboard shortcuts

--- keyboard-shortcuts/install.sh
+++ keyboard-shortcuts/install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 defaults write com.apple.universalaccess com.apple.custommenu.apps -array "com.apple.Music" "com.bitwarden.desktop" "com.apple.mail" "com.tinyspeck.slackmacgap"
+
 defaults write com.apple.Music NSUserKeyEquivalents '{
   "Go to Current Song"="@\$j";
   "Show Album in Library"="\Uf706";
@@ -8,14 +9,17 @@ defaults write com.apple.Music NSUserKeyEquivalents '{
   "Love"="@l";
   "Loved"="@l";
 }'
+
 defaults write com.bitwarden.desktop NSUserKeyEquivalents '{
   "Sync Vault"="@r";
 }'
+
 defaults write com.apple.mail NSUserKeyEquivalents '{
   "Message Viewer"="\U200b";
   "Hide Sidebar"="@0";
   "Show Sidebar"="@0";
 }'
+
 defaults write com.tinyspeck.slackmacgap NSUserKeyEquivalents '{
   "Actual Size"="\U200b";
   "Hide Sidebar"="@0";
